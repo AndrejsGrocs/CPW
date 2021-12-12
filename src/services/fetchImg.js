aa83dca47c80d2d3e49a1c24f5d0a2787f09fb57
Lars Nemeth <lars.nemeth@posteo.de>
2021-12-12 15:06:35 +0100

    no fetch till now

---
+++ src/services/fetchImg.js
@@ -0,0 +1,18 @@
+// import axios from 'axios';
+
+// const fetchImg = async () => {
+//   try {
+//     const response = await axios
+//       .get(`https://source.unsplash.com/199x200/?apple`)
+//       .then((response) => response.data);
+//     // const data = await response.data;
+//     console.log('My unsplash API from getCityImage');
+
+//     // return data;
+//   } catch (error) {
+//     throw error;
+//   }
+
+// };
+
+// export default fetchImg
--- src/services/nutritionApi.js
+++ src/services/nutritionApi.js
@@ -10,20 +10,23 @@ function nutritionApi() {
     params: { ingr: '1 large apple' },
     headers: {
       'x-rapidapi-host': 'edamam-edamam-nutrition-analysis.p.rapidapi.com',
-      'x-rapidapi-key': `${process.env.API_KEY}`,
+      // 'x-rapidapi-key': `${process.env.API_KEY}`,
+      'x-rapidapi-key': `be3edc6362msh395de5136f1c125p1e9d79jsn0e94ebe79825`,
     },
   };
 
   axios
     .request(options)
     .then(function (response) {
-      console.log(response.data);
+      console.log(response.data.calories);
+
     })
     .catch(function (error) {
       console.error(error);
     });
 
   return <div></div>;
+
 }
 
 export default nutritionApi;
