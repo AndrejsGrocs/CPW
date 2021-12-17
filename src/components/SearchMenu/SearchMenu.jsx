246e97c2001cb06ef2c51918c22b251c3728646a
Peony Langner <peony.langner@hotmail.de>
2021-12-17 16:30:36 +0100

    Fixed crashing

--- src/components/SearchMenu/SearchMenu.jsx
+++ src/components/SearchMenu/SearchMenu.jsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import {bootstrap} from 'react-bootstrap'
-
-export default function SearchMenu() {
-    return (
-        <div className='container'>
-        
-        <div className="input-group">
-  <div className="input-group-text">
-    <input className="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input"/>
-   
-  </div>
-  <input type="text" className="form-control" aria-label="Text input with radio button"/> 
-</div>
-<div className="input-group">
-  <div className="input-group-text">
-    <input className="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input"/>
-  </div>
-  <input type="text" className="form-control" aria-label="Text input with radio button"/>
-</div>
-<div className="input-group">
-  <div className="input-group-text">
-    <input className="form-check-input mt-0" type="radio" value="" aria-label="Radio button for following text input"/>
-  </div>
-  <input type="text" className="form-control" aria-label="Text input with radio button"/>
-</div>
-<div className="input-group input-group-lg">
-  <span className="input-group-text" id="inputGroup-sizing-lg">SEARCH ME HERE => </span>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
-</div>
-<div className='m-5 d-flex justify-content-evenly'>
-<button type="button" className="btn btn-primary btn-lg">SEARCH!</button>
-</div>
-</div>
-    )
-=======
 import { useState, useEffect } from 'react';
 import { getUnsplashData } from '../../services/apiServices';
 
@@ -83,5 +46,4 @@ export default function SearchMenu({ image, setImage, setImageData }) {
       </form>
     </div>
   );
->>>>>>> ec772528300a475f3daa3c6ab89b2fdf07b8da3a
 }
