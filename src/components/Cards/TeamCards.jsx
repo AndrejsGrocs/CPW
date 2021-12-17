d4ae1574bb192175b94b846cc601b8ecf0fa51a7
Robin Skladny <robin_skladny@gmx.de>
2021-12-17 12:13:32 +0100

    first AboutUs view solution

---
+++ src/components/Cards/TeamCards.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+
+export default function TeamCards() {
+  return (
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-end">
+        <div class="card " style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn ">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-center">
+        <div class="card" style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn  mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-start">
+        <div class="card" style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn ">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* second part of cards */}
+      <br />
+      <div class="col-md-4 d-flex justify-content-end">
+        <div class="card" style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-center">
+        <div class="card" style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-start">
+        <div class="card" style={{ width: "21rem" }}>
+          <img
+            src="https://i.imgur.com/ZTkt4I5.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn mr-2">
+              <i class="fas fa-link"></i> Visit Site
+            </a>
+            <a href="#" class="btn">
+              <i class="fab fa-github"></i> Github
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    // </div>
+  );
+}
