fcc00ee79481c5081a5f4cfca97a4740aeea4b06
Lars Nemeth <lars.nemeth@posteo.de>
2021-12-30 18:42:23 +0100

    nutritiousAmpel works fine

--- src/components/NutritiousAmpel/NutritiousAmpel.jsx
+++ src/components/NutritiousAmpel/NutritiousAmpel.jsx
@@ -6,13 +6,13 @@ import NutritionFacts from '../NutritionFacts/NutritionFacts';
 import nutritionApi from '../../services/NutritionApi';
 
 export default function NutritiousAmpel({ facts, setFacts }) {
-  const nutriAmpFunction = async () => {
-    const data = await nutritionApi();
-    setFacts(data);
-    console.log('my data from Ampel', data);
-
-    if (1 < 2) {
+  const nutriAmpFunction = () => {
+    if (facts.calories < 119) {
       return picA;
+    }
+
+    if (facts.calories > 120) {
+      return picB;
     }
   };
 
