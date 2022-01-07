b9884ddbd8cdc04c45803eb2dcb2df5726c62754
Lars Nemeth <lars.nemeth@posteo.de>
2022-01-07 16:21:20 +0100

    changed Ingredients Number

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,9 +4,9 @@ import NutritionFacts from '../NutritionFacts/NutritionFacts';
 
 export default function Card({ facts, setFacts, imageFromEdamam }) {
   return (
-    <div className='container mainCard'>    
+    <div className="container mainCard">
       <figure className="image-block row p-5 m-3">
-      <img
+        <img
           className="card-image col"
           src={
             imageFromEdamam
@@ -14,13 +14,15 @@ export default function Card({ facts, setFacts, imageFromEdamam }) {
               : 'https://www.frischeparadies-shop.de/media/image/0d/5c/66/OS_20122_1_Ananas.jpg'
           }
         />
-      <div className='col'>
-         <h3 className='fs-1'>Your Product is: <span className='variable-text'>Pineapple</span></h3> 
-         <h3 className='fs-2'>Health rating is:</h3>
+        <div className="col">
+          <h3 className="fs-1">
+            Your Product is: <span className="variable-text">Pineapple</span>
+          </h3>
+          <h3 className="fs-2">Health rating is:</h3>
           <figcaption>
-          <NutritionFacts facts={facts} setFacts={setFacts} />
-        </figcaption>
-    </div>     
+            <NutritionFacts facts={facts} setFacts={setFacts} />
+          </figcaption>
+        </div>
       </figure>
     </div>
   );
