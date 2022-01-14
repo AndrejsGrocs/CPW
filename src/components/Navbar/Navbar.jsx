7ee1ac37367a211cff3dd92d1528ba130193688c
Lukasz Cane <lukastrz6@gmail.com>
2022-01-14 15:24:52 +0100

    lukas

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,10 @@ import {NavLink} from 'react-router-dom'
 
 export default function Navbar() {
     return (
-<<<<<<< HEAD
         <div className="maincontainernavbar" >
-            <nav className="navbar navbar-expand-lg py-3 navbar-dark shadow-">
+            <nav className=" navbar-default navbar-fixed-top">
   <div className="container-fluid">
-    <a className="navbar-brand   col-lg text-uppercase font-weight-bold" href="#">CWP</a>
+    <a className="logo-tex navbar-brand   col-lg text-uppercase font-weight-bold" href="#">H</a>
     
   
       <div className="  navbar-nav  nav-list">
@@ -22,22 +21,7 @@ export default function Navbar() {
   </div>
 </nav>
         </div>
-=======
-        <nav className='nav'>
-            <div>
-              CPW Food Health App
-         </div>
-         <ul className='nav-list'>
-             <li> <NavLink to='/' exact activeClassName='active'>Landing</NavLink></li>
-             <li> <NavLink to='/aboutus' exact activeClassName='active'>About Us</NavLink></li>
-             {/* <li> <NavLink to='/cats/' exact activeClassName='active'>Cats</NavLink></li>
-             <li> <NavLink to='/dogs' exact activeClassName='active'>Dogs</NavLink></li> */}
-         </ul>
-
-
-        </nav>
->>>>>>> 42c7ba7d716278b87ecb443aa55c482d8fd8a495
-    )
+    );
 }
 
  
