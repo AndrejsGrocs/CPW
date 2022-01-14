51d1f02a0f3d9e9314c7da11ae29258cf41763bd
Lukasz Cane <lukastrz6@gmail.com>
2022-01-14 16:33:34 +0100

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import {NavLink} from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav className='nav'>
-            <div>
-              CPW Food Health App
+            <div className="logo-brand">
+              h
          </div>
          <ul className='nav-list'>
-             <li> <NavLink to='/' exact activeClassName='active'>Landing</NavLink></li>
+             <li> <NavLink to='/' exact activeClassName='active p-5'>Landing</NavLink></li>
              <li> <NavLink to='/aboutus' exact activeClassName='active'>About Us</NavLink></li>
              {/* <li> <NavLink to='/cats/' exact activeClassName='active'>Cats</NavLink></li>
              <li> <NavLink to='/dogs' exact activeClassName='active'>Dogs</NavLink></li> */}
