458a29ad021c0ac66055a0b8b3342db8d6a32412
Lukasz Cane <lukastrz6@gmail.com>
2022-01-24 14:56:31 +0100

    updates for navbar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
-            <span class="span-text">Copyright CPW </span>
+        <footer className="shadow-lg p-3  rounded">
+            <span className="span-text">Copyright CPW </span>
         </footer>
     )
 }
