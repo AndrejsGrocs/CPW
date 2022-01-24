0d623d0e5d752bfb5f547e01cc9adc6e0f87ea06
Lukasz Cane <lukastrz6@gmail.com>
2022-01-24 15:11:54 +0100

    updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="shadow-lg p-3  rounded">
+        <footer className="shadow-lg p-4  rounded">
             <span className="span-text">Copyright CPW </span>
         </footer>
     )
