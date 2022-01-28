99b69fd603835cf4672fd6903e93c1b0da4ef198
Peony Langner <peony.langner@hotmail.de>
2022-01-28 13:32:38 +0100

    Added .env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./assets/scss/main.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/scss/main.scss';
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
